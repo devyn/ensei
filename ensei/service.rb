baeb9e7d8aa52d195690dd6c63a6a03a9eb33921
linuxdevil <linuxdevil@6061cee6-f20f-45c5-a5ca-e397b9bf48f2>
2008-04-12 01:31:10 +0000

    update to Ensei: service now complete. skinning now complete.
    git-svn-id: http://svn.sharesource.org/svn/dcware/Ensei@9 6061cee6-f20f-45c5-a5ca-e397b9bf48f2

--- ensei/service.rb
+++ ensei/service.rb
@@ -3,24 +3,27 @@ require 'rexml/document'
 require 'open-uri'
 require 'cgi'
 require 'json'
+require 'skin'
 
 module Ensei; end unless defined?(Ensei) # declare Ensei
-class Ensei::EnseiError < Exception; end
+class Ensei::EnseiError < Exception; end unless defined?(Ensei::EnseiError)
 
 # this is for Ensei service files
 class Ensei::Service
 	def initialize(service_uri)
-		@uri = URI.parse(service_uri.class == URI ? service_uri.to_s : service_uri)
+		@uri = URI.parse(service_uri.class == URI ? service_uri.to_s : service_uri).to_s
 	end
 	def get_format
 		root = REXML::Document.new(open(@uri).read).root rescue return false
+		return false unless root.name =~ /^enseiServiceFormat$/i
 		root.elements.collect{|x|x.name}
 	end
 	def retrieve(params)
 		params.class >= Hash ? nil : raise 'parameters must be hash'
-		uri = @uri.dup
+		uri = URI.parse(@uri)
 		uri.query = make_query(params)
-		root = REXML::Document.new(open(uri).read).root rescue return false
+		root = REXML::Document.new(open(uri.to_s).read).root rescue return false
+		return false unless root.name =~ /^enseiService$/i
 		if (o = root.attributes.select{|x, v| x =~ /^enseiError$/i}).size > 0
 			raise EnseiError, root.attributes[o[0]]
 		end
@@ -33,14 +36,25 @@ class Ensei::Service
 		end
 		h
 	end
-	def to_proto_js(winId, backTo, timeout, params={})
-		return(<<-EOF)
-			function update_#{winId}() {
-				var resp = new Ajax.Request('#{backTo}', {parameters:#{params.to_json}});
-				eval(resp.responseText);
-			}
-			setTimeout(update_#{winId}, #{timeout.to_i});
-		EOF
+	def html(params=nil, formRequestURL=nil, winID=nil)
+		if params
+			retrieved_data = retrieve params
+			return Ensei::Skin.render(open(retrieved_data['skin']).read, retrieved_data)
+		elsif formRequestURL and winID
+			fields = get_format
+			cNum = winID
+			script = "function x#{cNum}_update() { Element.update('#{cNum}', new Ajax.Request('#{formRequestURL}', {method:'post', postBody:{"
+			str = "<form>"
+			fields.each_with_index do |fi, ind|
+				script << "#{fi}: document.evaluate('//input[id='#{cNum}_#{fi}']', document, null, XPathResult.ANY_TYPE, null).iterateNext().value#{"," unless ind == (fields.size - 1)} "
+				str << "#{CGI.escapeHTML(fi)}: <input id='#{cNum}_#{fi}'/><br/>"
+			end
+			script << "}}).responseText); }"
+			form << "<a href='javascript:x#{cNum}_update()'>load window</a></form>"
+			return "<script>#{script}</script>#{form}"
+		else
+			raise ArgumentError, "Must provide parameters or formRequestURL."
+		end
 	end
 	private; def make_query(h)
 		str = ""
---
+++ ensei/skin.rb
@@ -0,0 +1,21 @@
+# Ensei Service Skin Rendering
+require 'rexml/document'
+require 'cgi'
+
+module Ensei; end unless defined?(Ensei) # declare Ensei
+class Ensei::EnseiError < Exception; end unless defined?(Ensei::EnseiError)
+
+module Ensei::Skin
+	def self.render(skin, data)
+		skin = REXML::Document.new(skin).root
+		return false unless skin.name =~ /^enseiServiceSkin$/i
+		html = ""
+		skin.elements.each do |e|
+			html << "<span style='"
+			e.attributes.each do |k,v|
+				html << "#{k}:#{v};"
+			end
+			html << "'>#{CGI.escapeHTML(data[e.name] or "")}</span>"
+		end
+	end
+end
