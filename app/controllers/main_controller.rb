99461ba370abe9f37120d55cb18fbe234e1fdb9b
linuxdevil <linuxdevil@6061cee6-f20f-45c5-a5ca-e397b9bf48f2>
2008-04-13 01:07:20 +0000

    Ensei is fully working now. Please feel free to create windows for it.
    git-svn-id: http://svn.sharesource.org/svn/dcware/Ensei@11 6061cee6-f20f-45c5-a5ca-e397b9bf48f2

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,11 @@
 class MainController < ApplicationController
 	def index;end
+	# sample Ensei windows
+	def rssLoader
+		require 'rss/2.0'
+		@feed = RSS::Parser.parse(Net::HTTP.get(URI.parse(params['uri'])))
+	end
+	def clock; end
+	def help; end
+	def menu; end
 end
--- public/javascripts/windowFramework.js
+++ public/javascripts/windowFramework.js
@@ -1,21 +1,25 @@
 
 // Ensei window framework
 
-function openService(uri) {
-	wid = newWindow("Loading...");
-	setContent(wid, new Ajax.Request(uri).responseText);
-	return wid;
+function openService(uri, title) {
+	var successFunc = function(t) { var wid = newWindow(title, uri); setContent(wid, t.responseText); };
+	new Ajax.Request(uri, {method:'get', onSuccess:successFunc})
 }
 
-function newWindow(title) {
-	wid = Math.round((Math.random() * 1000000000));
-	new Insertion.Top('desktop', "<div class='window' id='window" + wid + "'><span class='titlebar' id='title" + wid + "'>" + title + "<span class='closeButton' onclick='closeWindow(" + wid + ")'>X</span></span><div class='content' id='" + wid + "'></div></div>");
+function newWindow(title, refreshURI) {
+	var wid = Math.round((Math.random() * 1000000000));
+	new Insertion.Top('desktop', "<div class='window' id='window" + wid + "'><span class='titlebar' id='title" + wid + "'>" + title + "</span><span class='refreshButton' onclick='refreshWindow("+wid+", \""+refreshURI+"\")'>R</span><span class='renameButton' onclick='setTitle("+wid+", prompt(\"rename to?\"))'>T</span><span class='closeButton' onclick='closeWindow(" + wid + ")'>X</span><div class='content' id='content" + wid + "'></div></div>");
 	new Draggable('window' + wid, {handle:'title'+wid});
 	return wid;
 }
 
+function refreshWindow(wid, refreshURI) {
+	var successFunc = function(t) { setContent(wid, t.responseText); };
+	new Ajax.Request(refreshURI, {method:'get', onSuccess:successFunc});
+}
+
 function setContent(wid, content) {
-	Element.update(""+wid+"", content);
+	Element.update("content"+ wid, content);
 }
 
 function getContent(wid) {
@@ -24,4 +28,8 @@ function getContent(wid) {
 
 function closeWindow(wid) {
 	Element.remove('window' + wid);
+}
+
+function setTitle(wid, title) {
+	Element.update("title"+ wid, title);
 }
